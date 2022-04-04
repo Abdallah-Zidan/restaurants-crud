983d7fbae8808916c1aac962520f03e724c99ea6
Abdallah-Zidan <eng.abdallahzidan@gmail.com>
2022-04-04 23:52:52 +0200

    implement required restaurants functionality in restaurants module

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -4,6 +4,8 @@ import * as mongoosSlugGenerator from 'mongoose-slug-generator';
 
 import { MongoSharedModule } from 'src/mongo-shared/mongo-shared.module';
 import { UsersModule } from 'src/users/users.module';
+import { RestaurantsController } from './restaurants.controller';
+import { RestaurantsService } from './restaurants.service';
 import { Restaurant, RestaurantSchema } from './schemas/restaurant.schema';
 
 @Module({
@@ -24,7 +26,7 @@ import { Restaurant, RestaurantSchema } from './schemas/restaurant.schema';
     MongoSharedModule,
     UsersModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [RestaurantsController],
+  providers: [RestaurantsService],
 })
 export class RestaurantsModule {}
---
+++ src/restaurants/restaurants.service.ts
@@ -0,0 +1,53 @@
+import { Model } from 'mongoose';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { Restaurant, RestaurantDocument } from './schemas/restaurant.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { RestaurantDto } from './dto/restaurant.dto';
+
+import {
+  dtoToRestaurant,
+  restaurantsToCollectionDto,
+} from './transformers/restaurant';
+import { SearchRestaurantsQuery } from './dto/search.dto';
+import { QueryService } from '../mongo-shared/query.service';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class RestaurantsService {
+  constructor(
+    @InjectModel(Restaurant.name)
+    private restaurantModel: Model<RestaurantDocument>,
+    private queryService: QueryService,
+    private userService: UsersService,
+  ) {
+    this.restaurantModel.ensureIndexes();
+    this.queryService.setGeospatialAttributes(['location']);
+  }
+
+  async create(body: RestaurantDto) {
+    const user = await this.findUserOrThrow(body.user);
+    const restaurant = await this.restaurantModel.create(dtoToRestaurant(body));
+    user.restaurants.push(restaurant);
+    await user.save();
+    return restaurant;
+  }
+
+  async find(id: string) {
+    return await this.queryService.getByIdOrSlug(id, this.restaurantModel);
+  }
+
+  async all(query: SearchRestaurantsQuery) {
+    query = this.queryService.buildQuery(query);
+    return this.queryService.paginate(
+      query,
+      this.restaurantModel,
+      restaurantsToCollectionDto,
+    );
+  }
+
+  private async findUserOrThrow(id: string) {
+    const user = await this.userService.find(id);
+    if (!user) throw new UnprocessableEntityException(["user doesn't exist"]);
+    return user;
+  }
+}
